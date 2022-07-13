fb2ae664249f53bffbbc3e96ac3a7ef70c24e507
Jay <biskyniffer@gmail.com>
2022-07-13 19:21:03 -0400

    getAllMoviesFromUserList

--- src/app/services/local-api.service.ts
+++ src/app/services/local-api.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Movie } from '../interfaces/movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalAPIService {
 
-  constructor() { }
+  constructor(private client:HttpClient) { }
+
+  //Get all movies from user list
+  GetAllMoviesFromUserList() : Observable<Movie[]>{
+    let movieList = this.client.get<Movie[]>(environment.apiUrl + "Movies/GetAllMoviesFromUserList");
+
+    return movieList;
+  }
 }
