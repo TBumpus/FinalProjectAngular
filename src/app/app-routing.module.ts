4cd0dfdeca850770e9c643d5906657ac20401fbc
Lonnie Johnson <lonnie.e.johnson@gmail.com>
2022-07-14 18:36:59 -0500

    All Movie List Component Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 import { AssignUpdateCategoryComponent } from './Components/assign-update-category/assign-update-category.component';
+import { MainComponent } from './Components/main/main.component';
 import { RandomMovieFromUserListByCategoryComponent } from './Components/random-movie-from-user-list-by-category/random-movie-from-user-list-by-category.component';
 import { RandomMovieFromUserListComponent } from './Components/random-movie-from-user-list/random-movie-from-user-list.component';
 import { SearchMovieResultComponent } from './Components/search-movie-result/search-movie-result.component';
@@ -9,13 +11,14 @@ import { UserMovieListComponent } from './Components/user-movie-list/user-movie-
 import { UserMoviesByCategoryComponent } from './Components/user-movies-by-category/user-movies-by-category.component';
 
 const routes: Routes = [
+  { path: "main", component: MainComponent},
   { path: '', redirectTo:'main', pathMatch: 'full'},
   { path:"assignUpdateCategory", component:AssignUpdateCategoryComponent},
   { path:"randomMovieFromUserList", component:RandomMovieFromUserListComponent},
   { path:"randomMovieFromUserListByCategory", component:RandomMovieFromUserListByCategoryComponent},
   { path:"searchMovie", component:SearchMovieComponent},
   { path:"searchMovieResult", component:SearchMovieResultComponent},
-  { path:"userMovieList", component:UserMovieListComponent},
+  { path:"userMovieList", component:UserMovieListComponent, canActivate:[AuthGuard]},
   { path:"userMoviesByCategory", component:UserMoviesByCategoryComponent}
 ];
 
