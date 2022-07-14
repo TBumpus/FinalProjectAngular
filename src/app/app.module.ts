8ae2e266a4378956768eb29063c558620133bf2e
Lonnie Johnson <lonnie.e.johnson@gmail.com>
2022-07-14 18:45:08 -0500

    AddNavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,9 +18,10 @@ import { RandomMovieFromUserListByCategoryComponent } from './Components/random-
 import { AssignUpdateCategoryComponent } from './Components/assign-update-category/assign-update-category.component';
 import { RandomMovieFromUserListComponent } from './Components/random-movie-from-user-list/random-movie-from-user-list.component';
 import { SearchMovieResultComponent } from './Components/search-movie-result/search-movie-result.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, AuthButtonComponent, UserProfileComponent, MainComponent, SearchMovieComponent, UserMovieListComponent, UserMoviesByCategoryComponent, RandomMovieFromUserListByCategoryComponent, AssignUpdateCategoryComponent, RandomMovieFromUserListComponent, SearchMovieResultComponent],
+  declarations: [AppComponent, AuthButtonComponent, UserProfileComponent, MainComponent, SearchMovieComponent, UserMovieListComponent, UserMoviesByCategoryComponent, RandomMovieFromUserListByCategoryComponent, AssignUpdateCategoryComponent, RandomMovieFromUserListComponent, SearchMovieResultComponent, NavbarComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
